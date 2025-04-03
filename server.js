c0e8616b0226fcdbf4de25ac4ce666cd8cc77d48
Diouldé Baldé <baldedioulde992@gmail.com>
2025-04-03 13:44:06 +0000

    2eme commit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Node.js!");
+  res.send(
+    "Vérifiez toutes vos définitions de routes et assurez-vous qu’aucun paramètre n’est vide!"
+  );
 });
 
 app.listen(PORT, () => {
